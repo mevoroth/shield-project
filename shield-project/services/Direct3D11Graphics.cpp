c24f91e4ceace4c569d28f4ccbd4adaf71fc38e9
Daniel Quach <darkskana@gmail.com>
2012-10-30 02:23:11 +0100

    Graphics service Shader implementation Device initialization implementation

--- shield-project/services/Direct3D11Graphics.cpp
+++ shield-project/services/Direct3D11Graphics.cpp
@@ -2,47 +2,128 @@
 
 #include <d3d11.h>
 #include <dxgi.h>
+#include <d3dcompiler.h>
 
 using namespace shield;
 
-services::Direct3D11Graphics::Direct3D11Graphics( HWND hwnd )
-	: _hwnd( hwnd ),
+services::Direct3D11Graphics::Direct3D11Graphics( HWND window )
+	: _window( window ),
 	_factory( 0 ),
 	_device( 0 ),
 	_deviceContext( 0 ),
-	_swapChain( 0 )
+	_swapChain( 0 ),
+	_renderTarget( 0 ),
+	_shaders( 0 )
 {
-	_initDevice();
-	_initSwapChain();
+	_shaders = new map<UINT16, ID3D11DeviceChild*>;
+
+	_initDeviceAndSwapChain();
+	_initRenderTarget();
+	_initViewport();
 };
-void services::Direct3D11Graphics::_initDevice()
+services::Direct3D11Graphics::~Direct3D11Graphics( void )
+{
+	_factory->Release();
+	_swapChain->Release();
+	_device->Release();
+	_deviceContext->Release();
+};
+template<class Shader> void services::Direct3D11Graphics::loadShader( UINT16 key, ID3D10Blob* shaderBlob )
+{
+	Shader* shader = 0;
+	pair<map<UINT16, ID3D11DeviceChild*>::const_iterator, bool> ret;
+
+	_loadShader( shaderBlob, shader );
+	ret = _shaders->insert(
+		pair<map<UINT16, ID3D11DeviceChild*>(
+			key,
+			shader
+		)
+	);
+	if ( !ret.second )
+	{
+		throw "Shader already inserted";
+	}
+};
+template<class Shader> void services::Direct3D11Graphics::useShader( UINT16 key )
+{
+	map<UINT16, ID3D11DeviceChild*>::const_iterator element = _shaders->find( key );
+	if ( element != _shaders->end() )
+	{
+		_useShader( dynamic_cast<Shader*>(element->second) );
+	}
+};
+void services::Direct3D11Graphics::_useShader( ID3D11VertexShader* shader )
+{
+	_deviceContext->VSSetShader( shader, 0, 0 );
+};
+void services::Direct3D11Graphics::_useShader( ID3D11HullShader* shader )
+{
+	_deviceContext->HSSetShader( shader, 0, 0 );
+};
+void services::Direct3D11Graphics::_useShader( ID3D11DomainShader* shader )
+{
+	_deviceContext->DSSetShader( shader, 0, 0 );
+};
+void services::Direct3D11Graphics::_useShader( ID3D11GeometryShader* shader )
+{
+	_deviceContext->GSSetShader( shader, 0, 0 );
+};
+void services::Direct3D11Graphics::_useShader( ID3D11PixelShader* shader )
+{
+	_deviceContext->PSSetShader( shader, 0, 0 );
+};
+void services::Direct3D11Graphics::_loadShader( ID3D10Blob* shaderBlob, ID3D11VertexShader*& shader )
+{
+	_device->CreateVertexShader(
+		shaderBlob->GetBufferPointer(),
+		shaderBlob->GetBufferSize(),
+		0,
+		&shader
+	);
+};
+void services::Direct3D11Graphics::_loadShader( ID3D10Blob* shaderBlob, ID3D11HullShader*& shader )
+{
+	_device->CreateHullShader(
+		shaderBlob->GetBufferPointer(),
+		shaderBlob->GetBufferSize(),
+		0,
+		&shader
+	);
+};
+void services::Direct3D11Graphics::_loadShader( ID3D10Blob* shaderBlob, ID3D11DomainShader*& shader )
+{
+	_device->CreateDomainShader(
+		shaderBlob->GetBufferPointer(),
+		shaderBlob->GetBufferSize(),
+		0,
+		&shader
+	);
+};
+void services::Direct3D11Graphics::_loadShader( ID3D10Blob* shaderBlob, ID3D11GeometryShader*& shader )
+{
+	_device->CreateGeometryShader(
+		shaderBlob->GetBufferPointer(),
+		shaderBlob->GetBufferSize(),
+		0,
+		&shader
+	);
+};
+void services::Direct3D11Graphics::_loadShader( ID3D10Blob* shaderBlob, ID3D11PixelShader*& shader )
+{
+	_device->CreatePixelShader(
+		shaderBlob->GetBufferPointer(),
+		shaderBlob->GetBufferSize(),
+		0,
+		&shader
+	);
+};
+void services::Direct3D11Graphics::_initDeviceAndSwapChain( void )
 {
 	D3D_FEATURE_LEVEL features[] = {
 		D3D_FEATURE_LEVEL_11_0,
 	};
 
-	D3D_FEATURE_LEVEL feature_returned;
-
-	HRESULT hr = D3D11CreateDevice(
-		0,					// Adapter (Carte graphique)
-		D3D_DRIVER_TYPE_HARDWARE,	// Driver
-		0,							// Software rasterizer
-		0,							// Flags
-		features,                   // Liste des versions
-		ARRAYSIZE(features),		// Taille de la liste des versions
-		D3D11_SDK_VERSION,			// SDK
-		&_device,					// Device
-		&feature_returned,			// Version choisie
-		&_deviceContext				// Device Context
-	);
-
-	if ( hr != S_OK )
-	{
-		throw "Impossible de créer le device Direct3D";
-	}
-};
-void services::Direct3D11Graphics::_initSwapChain()
-{
 	DXGI_SWAP_CHAIN_DESC swapChainDesc;
 
 	swapChainDesc.BufferDesc.Width						= WIDTH;
@@ -51,34 +132,93 @@ void services::Direct3D11Graphics::_initSwapChain()
 	swapChainDesc.BufferDesc.RefreshRate.Denominator	= FRAMERATE_DEN;
 	swapChainDesc.BufferDesc.ScanlineOrdering			=
 		DXGI_MODE_SCANLINE_ORDER_PROGRESSIVE;
-	swapChainDesc.BufferDesc.Format						= DXGI_FORMAT_R8G8B8A8_UINT;
-	swapChainDesc.BufferDesc.Scaling					= DXGI_MODE_SCALING_CENTERED;
+	swapChainDesc.BufferDesc.Format						=
+		DXGI_FORMAT_R8G8B8A8_UNORM;
+	swapChainDesc.BufferDesc.Scaling					=
+		DXGI_MODE_SCALING_STRETCHED;
 
 	swapChainDesc.SampleDesc.Count		= 1;
 	swapChainDesc.SampleDesc.Quality	= 0;
 
 	swapChainDesc.BufferUsage	= DXGI_USAGE_RENDER_TARGET_OUTPUT;
-	swapChainDesc.BufferCount	= 0;
-	swapChainDesc.OutputWindow	= _hwnd;
+	swapChainDesc.BufferCount	= 1;
+	swapChainDesc.OutputWindow	= _window;
 	swapChainDesc.Windowed		= false;
 	swapChainDesc.SwapEffect	= DXGI_SWAP_EFFECT_DISCARD;
 	swapChainDesc.Flags			= 0;
 
-	HRESULT hr = _getFactory()->CreateSwapChain(
-		_device,
-		&swapChainDesc,
-		&_swapChain
+	D3D_FEATURE_LEVEL feature_returned;
+
+	HRESULT hr = D3D11CreateDeviceAndSwapChain(
+		0,							// Adapter (Carte graphique)
+		D3D_DRIVER_TYPE_HARDWARE,	// Driver
+		0,							// Software rasterizer
+		0,							// Flags
+		features,                   // Liste des versions
+		ARRAYSIZE(features),		// Taille de la liste des versions
+		D3D11_SDK_VERSION,			// SDK
+		// Swap chain
+		&swapChainDesc,				// Description du swap chain
+		&_swapChain,				// Swap chain
+		// Device
+		&_device,					// Device
+		&feature_returned,			// Version choisie
+		&_deviceContext				// Device Context
 	);
 
-	/* if ( !hr )
+	if ( FAILED(hr) )
 	{
-	} */
+		throw "Impossible de créer le device Direct3D et le swap chain";
+	}
 };
-IDXGIFactory* services::Direct3D11Graphics::_getFactory()
+void services::Direct3D11Graphics::_initRenderTarget( void )
+{
+	ID3D11Texture2D* buffer = 0;
+
+	HRESULT hr = _swapChain->GetBuffer(
+		0,
+		__uuidof(ID3D11Texture2D),
+		(void**)&buffer
+	);
+
+	if ( FAILED(hr) )
+	{
+		throw "Impossible de récupérer le buffer du swap chain";
+	}
+
+	hr = _device->CreateRenderTargetView(
+		buffer,
+		0,
+		&_renderTarget
+	);
+	buffer->Release();
+
+	_deviceContext->OMSetRenderTargets(
+		1,
+		&_renderTarget,
+		0	// Depth stencil buffer
+	);
+};
+void services::Direct3D11Graphics::_initViewport( void )
+{
+	D3D11_VIEWPORT viewport;
+	viewport.TopLeftX = 0;
+	viewport.TopLeftY = 0;
+	viewport.Height	= HEIGHT;
+	viewport.Width	= WIDTH;
+	viewport.MinDepth = 0;
+	viewport.MaxDepth = 0;
+
+	_deviceContext->RSSetViewports(
+		1,
+		&viewport
+	);
+};
+IDXGIFactory* services::Direct3D11Graphics::_getFactory( void )
 {
 	if ( _factory == 0 )
 	{
-		CreateDXGIFactory1(
+		CreateDXGIFactory(
 			__uuidof(IDXGIFactory),
 			(void**)(&_factory)
 		);
--- shield-project/services/Direct3D11Graphics.h
+++ shield-project/services/Direct3D11Graphics.h
@@ -1,32 +1,89 @@
 #ifndef _DIRECT3D11GRAPHICS_H_
 #define _DIRECT3D11GRAPHICS_H_
 
+#include <map>
 #include <d3d11_1.h>
 #include "IGraphics.h"
 
+using namespace std;
+
 namespace shield {
 namespace services {
+	/**
+	 * For more information about graphic pipeline, see :
+	 * http://msdn.microsoft.com/en-us/library/windows/desktop/bb205123(v=vs.85).aspx
+	 */
 	class Direct3D11Graphics : public IGraphics
 	{
 	public:
-		enum {
-			DEFAULT_ADAPTER	= 0,
+		enum
+		{
 			WIDTH			= 1920,
 			HEIGHT			= 1080,
 			FRAMERATE_NUM	= 60,
 			FRAMERATE_DEN	= 1
 		};
 		Direct3D11Graphics( HWND );
+		~Direct3D11Graphics( void );
+		template<class Shader> void loadShader( UINT16, ID3D10Blob* );
+		template<class Shader> void useShader( UINT16 );
 
 	private:
-		HWND _hwnd;
+		HWND _window;
 		IDXGIFactory* _factory;
 		ID3D11Device* _device;
 		ID3D11DeviceContext* _deviceContext;
 		IDXGISwapChain* _swapChain;
-		IDXGIFactory* _getFactory();
-		void _initDevice();
-		void _initSwapChain();
+		ID3D11RenderTargetView* _renderTarget;
+		/**
+		 * Bibliothèque de shaders
+		 */
+		map<UINT16, ID3D11DeviceChild*>* _shaders;
+
+		IDXGIFactory* _getFactory( void );
+		void _initDeviceAndSwapChain( void );
+		void _initRenderTarget( void );
+		void _initViewport( void );
+		/**
+		 * Load vertex shader
+		 */
+		void _loadShader( ID3D10Blob*, ID3D11VertexShader*& );
+		/**
+		 * Load hull shader
+		 */
+		void _loadShader( ID3D10Blob*, ID3D11HullShader*& );
+		/**
+		 * Load vertex shader
+		 */
+		void _loadShader( ID3D10Blob*, ID3D11DomainShader*& );
+		/**
+		 * Load geometry shader
+		 */
+		void _loadShader( ID3D10Blob*, ID3D11GeometryShader*& );
+		/**
+		 * Load pixel shader
+		 */
+		void _loadShader( ID3D10Blob*, ID3D11PixelShader*& );
+		/**
+		 * Use vertex shader
+		 */
+		void _useShader( ID3D11VertexShader* );
+		/**
+		 * Use hull shader
+		 */
+		void _useShader( ID3D11HullShader* );
+		/**
+		 * Use domain shader
+		 */
+		void _useShader( ID3D11DomainShader* );
+		/**
+		 * Use geometry shader
+		 */
+		void _useShader( ID3D11GeometryShader* );
+		/**
+		 * Use pixel shader
+		 */
+		void _useShader( ID3D11PixelShader* );
 	};
 };
 };
